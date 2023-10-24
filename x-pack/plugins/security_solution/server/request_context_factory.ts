add820e1dd24cc1ba7e114d9fedc6d7542d8331a
Marshall Main <marshall.main@elastic.co>
2023-10-23 17:20:03 -0700

    Add API tests, change field name, fix route to use existing services provider

--- x-pack/plugins/security_solution/server/request_context_factory.ts
+++ x-pack/plugins/security_solution/server/request_context_factory.ts
@@ -9,8 +9,6 @@ import { memoize } from 'lodash';
 
 import type { Logger, KibanaRequest, RequestHandlerContext } from '@kbn/core/server';
 
-import type { AuthenticatedUser } from '@kbn/security-plugin/common';
-
 import { DEFAULT_SPACE_ID } from '../common/constants';
 import { AppClientFactory } from './client';
 import type { ConfigType } from './config';
@@ -144,8 +142,6 @@ export class RequestContextFactory implements IRequestContextFactory {
             dataStreamAdapter: plugins.alerting.getDataStreamAdapter(),
           })
       ),
-
-      getUser: (): AuthenticatedUser | null => startPlugins.security.authc.getCurrentUser(request),
     };
   }
 }
